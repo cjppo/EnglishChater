16badee967c103e1d1af22febb8505825537efa1
jie.cao <jiec@squareup.com>
2023-03-09 18:21:32 +0800

    close debug mode

--- englishchater/settings.py
+++ englishchater/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$x7p6l@uztnc@br@9p!o*4pm53a#@ha-r-_@j28frt@_kty=06'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
